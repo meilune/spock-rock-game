35fb584724fdc867598f255ce669c3846b9b9836
Ginja <virginija.meilune@gmail.com>
2022-09-05 06:18:39 +1000

    Adding basic JS set up

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
+const playerScoreEl = document.getElementById('player-score');
+const playerChoiceEl = document.getElementById('player-choice');
+const computerScoreEl = document.getElementById('computer-score');
+const computerChoiceEl = document.getElementById('computer-choice');
+const resultText = document.getElementById('result-text');
 
+const playerRock = document.getElementById('player-rock');
+const playerPaper = document.getElementById('player-paper');
+const playerScissors = document.getElementById('player-scissors');
+const playerLizard = document.getElementById('player-lizard');
+const playerSpock = document.getElementById('player-spock');
+
+const computerRock = document.getElementById('computer-rock');
+const computerPaper = document.getElementById('computer-paper');
+const computerScissors = document.getElementById('computer-scissors');
+const computerLizard = document.getElementById('computer-lizard');
+const computerSpock = document.getElementById('computer-spock');
+
+const allGameIcons = document.querySelectorAll(".fa-regular");
 
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
